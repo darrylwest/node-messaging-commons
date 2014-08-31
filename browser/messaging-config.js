d237a6f0e7247a4c469007a23593a82a57ce420d
darryl west <darryl.west@RainCitySoftware.com>
2014-08-31 11:40:15 -0700

    added browser messaging commons and config

---
+++ browser/messaging-config.js
@@ -0,0 +1,13 @@
+
+var readMessagingConfig = function() {
+    var config = {
+            "socketHost":"http://localhost:29169",
+            "port":29169,
+            "hubName":"/MessageHub",
+            "algorithm":"sha256",
+            "appkey":"d6527612-55e9-4e10-8d4c-2979a979928a"
+        };
+
+    return config;
+};
+
